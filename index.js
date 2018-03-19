846a7a38bb405e7862316bac35d5a609a2a543cb
intern003 <intern003@50-64727.local>
2018-03-19 16:27:55 +0900

    show list of response

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ fetch(url, {
 .then(response => {
   console.log('Success:', response);
   elem = document.getElementById('container');
-  elem.innerHTML = response.items[0].full_name;
+  let tags = '';
+  for(var i=0; i< response.items.length; i++){
+    tags += '<div>' + response.items[i].full_name + '</div>';
+  }
+  elem.innerHTML = tags;
 });
 
 // memo
