cb5a9e96c12e4164928b3547087e51e941c9cebf
intern003 <intern003@50-64727.local>
2018-03-19 16:03:01 +0900

    add http request

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+// // console.log("hello world");
+var url = 'https://api.github.com/search/repositories?q=ruby&sort=stars';
+
+fetch(url, {
+  method: 'GET'
+}).then(res => res.json())
+.catch(error => console.error('Error:', error))
+.then(response => {
+  console.log('Success:', response);
+  elem = document.getElementById('container');
+  elem.innerHTML = response.items[0].full_name;
+});
+
+// memo
+// debugger
