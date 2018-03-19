a2860bcab4bed2503399df99ac93b6de26e012fd
intern003 <intern003@50-64727.local>
2018-03-19 17:19:31 +0900

    add user search

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-// // console.log("hello world");
-var url = 'https://api.github.com/search/repositories?q=ruby&sort=stars';
-
-fetch(url, {
-  method: 'GET'
-}).then(res => res.json())
-.catch(error => console.error('Error:', error))
-.then(response => {
-  console.log('Success:', response);
-  elem = document.getElementById('container');
-  let tags = '';
-  for(var i=0; i< response.items.length; i++){
-    tags += '<div>' + response.items[i].full_name + '</div>';
-  }
-  elem.innerHTML = tags;
+button.addEventListener("click", function(e) {
+  var textForm = document.getElementById("textForm").value;
+  var url = 'https://api.github.com/search/repositories?q=' + textForm + '&sort=stars';
+  fetch(url, {
+    method: 'GET'
+  }).then(res => res.json())
+  .catch(error => console.error('Error:', error))
+  .then(response => {
+    console.log('Success:', response);
+    var button = document.getElementById("button");
+    elem = document.getElementById('container');
+    let tags = '';
+    for(var i=0; i< response.items.length; i++){
+      tags += '<div>' + response.items[i].full_name + '</div>';
+    }
+    elem.innerHTML = tags;
+  });
 });
 
+// // console.log("hello world");
 // memo
 // debugger
