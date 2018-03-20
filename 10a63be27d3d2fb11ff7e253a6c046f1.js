3f8d016631e872c92fd9375b1920101ed832ab2a
intern003 <intern003@50-64727.local>
2018-03-20 18:32:37 +0900

    Updates

--- 10a63be27d3d2fb11ff7e253a6c046f1.js
+++ 10a63be27d3d2fb11ff7e253a6c046f1.js
@@ -138,7 +138,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":4}],6:[function(require,module,exports) {
+},{"_css_loader":4}],14:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '59746' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '63309' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -261,5 +261,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[6])
+},{}]},{},[14])
 //# sourceMappingURL=/dist/10a63be27d3d2fb11ff7e253a6c046f1.map
